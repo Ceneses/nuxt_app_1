f024e48fa1d6bddf5e52c0b10bd5172ba602633f
恒成立 <wo51study@163.com>
2024-01-26 20:19:47 +0800

    [优化] 1.版本变更

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :class="{'dark': darkMode}">
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const darkMode = ref(false)
+</script>
---
+++ components/Sidebar/Right.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  右边栏内容
+</div>
+</template>
+
+<script>
+export default {
+  name: "Right"
+};
+</script>
+
+<style scoped>
+
+</style>
